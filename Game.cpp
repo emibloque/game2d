2d2facb51d78ac91d8a2905bddf6f71cefdcd699
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2021-09-16 17:15:30 +0200

    Add GameLayer

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,6 @@
 
 #include "Game.h"
+#include "GameLayer.h"
 
 Game::Game()
 {
@@ -16,7 +17,7 @@ Game::Game()
 	// https://wiki.libsdl.org/SDL_HINT_RENDER_SCALE_QUALITY
 	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
 
-	gameLayer = new Layer(this);
+	gameLayer = new GameLayer(this);
 
 	loopActive = true; // bucle activo
 	loop();
